2ec3bc14b4328a315e0b7ccaed4c0f95925116b2
siva1029 <ummareddy.wd@gmail.com>
2025-02-06 15:49:32 +0530

    get_data code updated and data pushed to mongodb

--- get_data.py
+++ get_data.py
@@ -26,10 +26,38 @@ class NetworkDataExtract():
             pass
         except Exception as e:
             raise NetworkSecurityException(e,sys)
-    def csv_to_json_converter(self):
+    def csv_to_json_converter(self,file_path):
         try:
-            pass
+            data=pd.read_csv(file_path)
+            data.reset_index(drop=True, inplace=True) #here resetting index
+            records=list(json.loads(data.T.to_json()).values())
+            return records
         except Exception as e:
             raise NetworkSecurityException(e,sys)
-    if __name__ == '__main__':
-        pass
+
+    def pushing_data_to_mongodb(self,records,database,collection):
+        try:
+            self.database=database
+            self.collection=collection
+            self.records=records
+            self.mongo_client = pymongo.MongoClient(MONGO_DB_URL)
+
+            self.database = self.mongo_client[self.database]
+            self.collection = self.database[self.collection]
+            self.collection.insert_many(self.records)
+            return len(self.records)
+
+
+
+        except Exception as e:
+            raise NetworkSecurityException(e,sys) 
+
+
+if __name__ == '__main__':
+    FILE_PATH='./Network_Data/NetworkData.csv'
+    DATABASE="KNAcadeny"
+    COLLECTION="NetworkData"
+    networkobj = NetworkDataExtract()
+    records = networkobj.csv_to_json_converter(FILE_PATH)
+    noofrecords=networkobj.pushing_data_to_mongodb(records,DATABASE,COLLECTION)
+    print(noofrecords)
