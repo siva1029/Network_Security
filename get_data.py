caa65458afc7978d92f24a5db8c4768770289594
siva1029 <ummareddy.wd@gmail.com>
2025-02-06 11:02:55 +0530

    setup code updated

--- get_data.py
+++ get_data.py
@@ -1,15 +1,15 @@
-import os
+import os #Why os grade out here?
 import sys
 import json
 
 from dotenv import load_dotenv
 
-
+#What is the meaning of word color codings?
 
 import certifi
 import pandas as pd
 import numpy as np
-import pymango
+import pymango #Why yellow line undersline?
 
 from networksecurity.exception.exception import NetwrokSecurityException
 from networksecurity.logger.logger import logging
