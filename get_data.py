d121fd0632f439e433751abba376fe0a5d699cc5
siva1029 <ummareddy.wd@gmail.com>
2025-02-06 10:23:32 +0530

    get_data code ready

---
+++ get_data.py
@@ -0,0 +1,29 @@
+import os
+import sys
+import json
+
+from dotenv import load_dotenv
+
+
+
+import certifi
+import pandas as pd
+import numpy as np
+import pymango
+
+from networksecurity.exception.exception import NetwrokSecurityException
+from networksecurity.logger.logger import logging
+
+class NetworkDataExtract():
+    def __init__(self):
+        try:
+            pass
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
+    def csv_to_json_converter(self):
+        try:
+            pass
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
+    if __name__ == '__main__':
+        pass
